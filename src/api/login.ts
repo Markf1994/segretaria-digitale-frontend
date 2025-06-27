e1211e6b6f3551f88b2389898f31a15092060db9
Markf1994 <markfenaroli@gmail.com>
2025-06-27 23:47:49 +0200

    Remove unused login API

--- src/api/login.ts
+++
@@ -1,8 +0,0 @@
-import api from './axios';
-export const login = async (email: string, password: string) => {
-  const form = new URLSearchParams();
-  form.append('email', email);
-  form.append('password', password);
-  const res = await api.post('/login', form);
-  return res.data.access_token;
-};
