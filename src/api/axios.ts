4c796668b7b8de09f18ac2a4e2acc0ec44a286b7
Markf1994 <markfenaroli@gmail.com>
2025-06-27 10:04:51 +0200

    feat: add auth interceptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,15 @@
 import axios from 'axios';
+import { useAuthStore } from '../store/auth';
+
 const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || '' });
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token || localStorage.getItem('token');
+  if (token) {
+    config.headers = config.headers || {};
+    (config.headers as any).Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
