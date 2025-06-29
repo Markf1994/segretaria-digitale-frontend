cb32b7597e377ad62928934fa6dfbdc261ece122
Markf1994 <markfenaroli@gmail.com>
2025-06-30 01:12:44 +0200

    feat: add loader while logging in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,10 +3,12 @@ import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 import { useAuthStore } from "../store/auth";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const setToken = useAuthStore(s => s.setToken);
 
@@ -20,12 +22,14 @@ const LoginPage: React.FC = () => {
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const res = await api.post("/login", { email, password });
       setToken(res.data.access_token);
       navigate("/");
     } catch {
       alert("Credenziali errate");
+      setLoading(false);
     }
   };
 
@@ -33,28 +37,32 @@ const LoginPage: React.FC = () => {
     <>
       <div className="login-page">
         <h1 className="login-title">Segretaria digitale Polizia Locale Castione della Presolana</h1>
-        <div className="login-card">
+      <div className="login-card">
           <img src="/logo.png" alt="Logo" className="login-logo" />
-          <form className="login-form" onSubmit={onSubmit}>
-          <input
-            id="login-email"
-            type="email"
-            placeholder="Email istituzionale"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-          <input
-            id="login-password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
-            <button type="submit">Accedi</button>
-          </form>
-        </div>
+          {loading ? (
+            <Loader />
+          ) : (
+            <form className="login-form" onSubmit={onSubmit}>
+              <input
+                id="login-email"
+                type="email"
+                placeholder="Email istituzionale"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+              <input
+                id="login-password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+              />
+              <button type="submit">Accedi</button>
+            </form>
+          )}
+      </div>
       </div>
       <Footer />
     </>
