0cd949522a2b2cb9fe70aab6940592b5aec1c49f
Markf1994 <markfenaroli@gmail.com>
2025-07-11 20:30:50 +0200

    Display Google Calendar colors on dashboard

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,16 @@
 export const DEFAULT_CALENDAR_ID = '9b868ea25bcd2be6f72fc415d45753a30abcc651070802054d21cfa9f5f97559@group.calendar.google.com';
 
+export const GOOGLE_COLOR_MAP: Record<string, string> = {
+  '1': '#a4bdfc',
+  '2': '#7ae7bf',
+  '3': '#dbadff',
+  '4': '#ff887c',
+  '5': '#fbd75b',
+  '6': '#ffb878',
+  '7': '#46d6db',
+  '8': '#e1e1e1',
+  '9': '#5484ed',
+  '10': '#51b749',
+  '11': '#dc2127',
+};
+
